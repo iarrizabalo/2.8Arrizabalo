b327c08d405e4c14f563371ceeabe6898c191b99
iarrizabalo <146086444+iarrizabalo@users.noreply.github.com>
2023-11-16 09:22:40 +0100

    Update kalkulagailua.java

--- src/kalkulagailua.java
+++ src/kalkulagailua.java
@@ -1,20 +1,10 @@
 public class kalkulagailua {
-    public int batuketa(int a, int b) {
-        return a + b;
+    public double inversoa(double a) {
+    if (a != 0) {
+        return 1 / a;
+    } else {
+        throw new ArithmeticException("Zero division error");
     }
+}
 
-    public int kenketa(int a, int b){
-        return a - b;
-    }
-
-    public int biderketa(int a, int b) {
-        return a * b;
-    }
-    public int zatiketa(int a, int b) {
-        if (b != 0) {
-            return a / b;
-        } else {
-            throw new ArithmeticException("errorea");
-        }
-    }
 }
